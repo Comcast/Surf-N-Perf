f665b6f5948fe08d18c14c7c86837125fc544d04
Filipe Araujo <filipe.alves.araujo@gmail.com>
2014-06-02 19:59:40 -0400

    optimizing code, replacing extend with prototypical inheritance instead

--- surfnperf.js
+++ surfnperf.js
@@ -64,262 +64,258 @@
     this.initialize();
   };
 
-  _.extend(SurfNPerf.prototype, {
-
-    _setPerformanceApis: function() {
-      if(window.performance) {
-        this._navigationTiming = !!(window.performance.timing);
-        this._highResTime = !!(window.performance.now);
-        this._userTiming = !!(window.performance.mark && window.performance.measure && window.performance.getEntriesByName);
-      } else {
-        this._navigationTiming = false;
-        this._highResTime = false;
-        this._userTiming = false;
-      }
-    },
-
-    _setPerfProperties: function() {
-      if(_.isUndefined(window.SURF_N_PERF) || !window.SURF_N_PERF.marks) {
-        window.SURF_N_PERF = {
-          marks: {},
-          highResMarks: {}
-        };
-      }
-    },
-
-    _setInitialUrl: function() {
-      this.setCustom('initialUrl', window.location.pathname);
-    },
-
-    initialize: function() {
-      this._setPerformanceApis();
-      this._setPerfProperties();
-      this._setInitialUrl();
-    },
-
-    /**
-     * Returns the timing data for a particular eventKey
-     *
-     * @arguments {String} timeType 'highRes' (to return a DOMHighResTimeStamp, if available) or 'DOM' (to return a DOMTimeStamp's value) - optional. Defaults to 'highRes'
-     * @returns {DOMHighResTimeStamp | integer} time value
-     * @memberOf SurfNPerf
-     */
-    now: function(timeType) {
-      timeType = timeType || 'highRes';
-      if(this._highResTime && timeType === 'highRes') {
-        return window.performance.now();
-      } else {
-        return Date.now();
-      }
-    },
-
-    performanceTiming: function() {
-      return this._navigationTiming ? window.performance.timing : {};
-    },
-
-    _performanceTimingL2: function(eventKey) {
-      var delta = this.getTimingMark('loadEventEnd', 'DOM') - this.getTimingMark(eventKey, 'DOM'),
-        value = window.SURF_N_PERF.highResMarks.loadEventEnd - delta;
-      return(value < 0) ? 0 : this._round(value, {
-        decimalPlaces: 10
-      });
-    },
-
-    /**
-     * Returns the timing data for a particular eventKey
-     *
-     * @arguments {String} eventKey name of the timing event
-     * @arguments {String} timeType 'highRes' (to return a DOMHighResTimeStamp, if available) or 'DOM' (to return a DOMTimeStamp's value) - optional. Defaults to 'DOM'
-     * @returns {DOMHighResTimeStamp | integer} time value
-     * @memberOf SurfNPerf
-     */
-    getTimingMark: function(eventKey, timeType) {
-      timeType = timeType || 'DOM';
-
-      if(this._navigationTiming) {
-        if(timeType === 'DOM' || this._highResTime === false) {
-          return this.performanceTiming()[eventKey];
-        } else { // timeType === 'HighRes'
-          return this._performanceTimingL2(eventKey);
-        }
-      } else {
-        if(_.contains(this._navigationTimingEvents.a, eventKey)) {
-          return this.getMark('pageStart', 'DOM');
-        } else {
-          return this.getMark('loadEventEnd', 'DOM');
-        }
-      }
-    },
-
-    userTiming: function() {
-      return this._userTiming ? window.performance : {};
-    },
-
-    mark: function(eventKey) {
-      if(this._highResTime) {
-        this._data.highResMarks[eventKey] = this.now();
-      }
-      if(this._userTiming) {
-        this.userTiming().mark(eventKey);
-      }
-      this._data.marks[eventKey] = this.now('DOM');
-    },
-
-    getMark: function(eventKey, timeType) {
-      var mark;
-
-      timeType = timeType || 'highRes';
-
-      if(timeType === 'highRes' && this._highResTime === true) {
-        mark = this._data.highResMarks[eventKey] || window.SURF_N_PERF.highResMarks[eventKey];
-      }
-      return mark || this._data.marks[eventKey] || window.SURF_N_PERF.marks[eventKey];
-    },
-
-    _isTimingMark: function(eventKey) {
-      return _.contains(this._navigationTimingEvents.a, eventKey) || _.contains(this._navigationTimingEvents.b, eventKey);
-    },
-
-    _getDurationMark: function(eventKey) {
-      if(this._isTimingMark(eventKey)) {
-        return this.getTimingMark(eventKey, 'highRes');
-      } else {
-        return this.getMark(eventKey);
-      }
-    },
-
-    _round: function(n, options) {
-      options = options || {};
-      var dp = options.decimalPlaces || 0;
-      return +(n).toFixed(dp);
-    },
-
-    _roundedDuration: function(a, b, options) {
-      return this._round(b - a, options);
-    },
-
-    _measureName: function(a, b) {
-      return '_SNP_' + a + '_TO_' + b;
-    },
-
-    _setMeasure: function(a, b) {
-      this.userTiming().measure(this._measureName(a, b), a, b);
-    },
-
-    _getMeasureDuration: function(a, b) {
-      var measure = this.userTiming().getEntriesByName(this._measureName(a, b))[0] || {};
-      return measure.duration;
-    },
-
-    duration: function(a, b, options) {
-      if(this._userTiming) {
-        this._setMeasure(a, b);
-        return this._round(this._getMeasureDuration(a, b), options);
-      } else {
-        return this._roundedDuration(this._getDurationMark(a), this._getDurationMark(b), options);
-      }
-    },
-
-    updateEvent: function(eventKey, key, value) {
-      var obj = {};
-      obj[eventKey] = {};
-
-      _.defaults(this._data.events, obj);
-
-      this._data.events[eventKey][key] = value;
-    },
-
-    resetEvent: function(eventKey, key, value) {
-      this._data.events[eventKey] = {};
-      this._data.events[eventKey][key] = value;
-    },
-
-    eventStart: function(eventKey) {
-      this.resetEvent(eventKey, 'start', this.now());
-    },
-
-    eventEnd: function(eventKey, options) {
-      var now = this.now();
-
-      options = options || {};
-
-      _.each(options, function(value, key) {
-        this.updateEvent(eventKey, key, value);
-      }, this);
-
-      this.updateEvent(eventKey, 'end', now);
-    },
-
-    getEventData: function(eventKey, key) {
-      var eventObj = this._data.events[eventKey];
-      if(eventObj) {
-        return eventObj[key];
-      }
-    },
-
-    eventDuration: function(eventKey, options) {
-      return this._roundedDuration(this.getEventData(eventKey, 'start'), this.getEventData(eventKey, 'end'), options);
-    },
-
-    setCustom: function(key, value) {
-      this._data.custom[key] = value;
-    },
-
-    getCustom: function(key) {
-      return this._data.custom[key];
-    },
-
-    /**
-     * Total time for App Cache, DNS & TCP
-     *
-     * @returns {integer} time in ms
-     * @memberOf SurfNPerf
-     */
-    getNetworkTime: function() {
-      return this.duration('fetchStart', 'connectEnd');
-    },
-
-    /**
-     * Total time for Request & Response
-     *
-     * @returns {integer} time in ms
-     * @memberOf SurfNPerf
-     */
-    getServerTime: function() {
-      return this.duration('requestStart', 'responseEnd');
-    },
-
-    /**
-     * Total time for App Cache, DNS, TCP, Request & Response
-     *
-     * @returns {integer} time in ms
-     * @memberOf SurfNPerf
-     */
-    getNetworkLatency: function() {
-      return this.duration('fetchStart', 'responseEnd');
-    },
-
-    /**
-     * Total time to process the Response & fire the onLoad event
-     *
-     * @returns {integer} time in ms
-     * @memberOf SurfNPerf
-     */
-    getProcessingLoadTime: function() {
-      return this.duration('responseEnd', 'loadEventEnd');
-    },
-
-    /**
-     * Total time for a page to load from the time the user initiates the access of the page to the firing of the onLoad event
-     *
-     * @returns {integer} time in ms
-     * @memberOf SurfNPerf
-     */
-    getFullRequestLoadTime: function() {
-      return this.duration('navigationStart', 'loadEventEnd');
+  SurfNPerf.prototype._setPerformanceApis = function() {
+    if(window.performance) {
+      this._navigationTiming = !!(window.performance.timing);
+      this._highResTime = !!(window.performance.now);
+      this._userTiming = !!(window.performance.mark && window.performance.measure && window.performance.getEntriesByName);
+    } else {
+      this._navigationTiming = false;
+      this._highResTime = false;
+      this._userTiming = false;
     }
+  };
 
-  });
+  SurfNPerf.prototype._setPerfProperties = function() {
+    if(_.isUndefined(window.SURF_N_PERF) || !window.SURF_N_PERF.marks) {
+      window.SURF_N_PERF = {
+        marks: {},
+        highResMarks: {}
+      };
+    }
+  };
+
+  SurfNPerf.prototype._setInitialUrl = function() {
+    this.setCustom('initialUrl', window.location.pathname);
+  };
+
+  SurfNPerf.prototype.initialize = function() {
+    this._setPerformanceApis();
+    this._setPerfProperties();
+    this._setInitialUrl();
+  };
+
+  /**
+   * Returns the timing data for a particular eventKey
+   *
+   * @arguments {String} timeType 'highRes' (to return a DOMHighResTimeStamp, if available) or 'DOM' (to return a DOMTimeStamp's value) - optional. Defaults to 'highRes'
+   * @returns {DOMHighResTimeStamp | integer} time value
+   * @memberOf SurfNPerf
+   */
+  SurfNPerf.prototype.now = function(timeType) {
+    timeType = timeType || 'highRes';
+    if(this._highResTime && timeType === 'highRes') {
+      return window.performance.now();
+    } else {
+      return Date.now();
+    }
+  };
+
+  SurfNPerf.prototype.performanceTiming = function() {
+    return this._navigationTiming ? window.performance.timing : {};
+  };
+
+  SurfNPerf.prototype._performanceTimingL2 = function(eventKey) {
+    var delta = this.getTimingMark('loadEventEnd', 'DOM') - this.getTimingMark(eventKey, 'DOM'),
+      value = window.SURF_N_PERF.highResMarks.loadEventEnd - delta;
+    return(value < 0) ? 0 : this._round(value, {
+      decimalPlaces: 10
+    });
+  };
+
+  /**
+   * Returns the timing data for a particular eventKey
+   *
+   * @arguments {String} eventKey name of the timing event
+   * @arguments {String} timeType 'highRes' (to return a DOMHighResTimeStamp, if available) or 'DOM' (to return a DOMTimeStamp's value) - optional. Defaults to 'DOM'
+   * @returns {DOMHighResTimeStamp | integer} time value
+   * @memberOf SurfNPerf
+   */
+  SurfNPerf.prototype.getTimingMark = function(eventKey, timeType) {
+    timeType = timeType || 'DOM';
+
+    if(this._navigationTiming) {
+      if(timeType === 'DOM' || this._highResTime === false) {
+        return this.performanceTiming()[eventKey];
+      } else { // timeType === 'HighRes'
+        return this._performanceTimingL2(eventKey);
+      }
+    } else {
+      if(_.contains(this._navigationTimingEvents.a, eventKey)) {
+        return this.getMark('pageStart', 'DOM');
+      } else {
+        return this.getMark('loadEventEnd', 'DOM');
+      }
+    }
+  };
+
+  SurfNPerf.prototype.userTiming = function() {
+    return this._userTiming ? window.performance : {};
+  };
+
+  SurfNPerf.prototype.mark = function(eventKey) {
+    if(this._highResTime) {
+      this._data.highResMarks[eventKey] = this.now();
+    }
+    if(this._userTiming) {
+      this.userTiming().mark(eventKey);
+    }
+    this._data.marks[eventKey] = this.now('DOM');
+  };
+
+  SurfNPerf.prototype.getMark = function(eventKey, timeType) {
+    var mark;
+
+    timeType = timeType || 'highRes';
+
+    if(timeType === 'highRes' && this._highResTime === true) {
+      mark = this._data.highResMarks[eventKey] || window.SURF_N_PERF.highResMarks[eventKey];
+    }
+    return mark || this._data.marks[eventKey] || window.SURF_N_PERF.marks[eventKey];
+  };
+
+  SurfNPerf.prototype._isTimingMark = function(eventKey) {
+    return _.contains(this._navigationTimingEvents.a, eventKey) || _.contains(this._navigationTimingEvents.b, eventKey);
+  };
+
+  SurfNPerf.prototype._getDurationMark = function(eventKey) {
+    if(this._isTimingMark(eventKey)) {
+      return this.getTimingMark(eventKey, 'highRes');
+    } else {
+      return this.getMark(eventKey);
+    }
+  };
+
+  SurfNPerf.prototype._round = function(n, options) {
+    options = options || {};
+    var dp = options.decimalPlaces || 0;
+    return +(n).toFixed(dp);
+  };
+
+  SurfNPerf.prototype._roundedDuration = function(a, b, options) {
+    return this._round(b - a, options);
+  };
+
+  SurfNPerf.prototype._measureName = function(a, b) {
+    return '_SNP_' + a + '_TO_' + b;
+  };
+
+  SurfNPerf.prototype._setMeasure = function(a, b) {
+    this.userTiming().measure(this._measureName(a, b), a, b);
+  };
+
+  SurfNPerf.prototype._getMeasureDuration = function(a, b) {
+    var measure = this.userTiming().getEntriesByName(this._measureName(a, b))[0] || {};
+    return measure.duration;
+  };
+
+  SurfNPerf.prototype.duration = function(a, b, options) {
+    if(this._userTiming) {
+      this._setMeasure(a, b);
+      return this._round(this._getMeasureDuration(a, b), options);
+    } else {
+      return this._roundedDuration(this._getDurationMark(a), this._getDurationMark(b), options);
+    }
+  };
+
+  SurfNPerf.prototype.updateEvent = function(eventKey, key, value) {
+    var obj = {};
+    obj[eventKey] = {};
+
+    _.defaults(this._data.events, obj);
+
+    this._data.events[eventKey][key] = value;
+  };
+
+  SurfNPerf.prototype.resetEvent = function(eventKey, key, value) {
+    this._data.events[eventKey] = {};
+    this._data.events[eventKey][key] = value;
+  };
+
+  SurfNPerf.prototype.eventStart = function(eventKey) {
+    this.resetEvent(eventKey, 'start', this.now());
+  };
+
+  SurfNPerf.prototype.eventEnd = function(eventKey, options) {
+    var now = this.now();
+
+    options = options || {};
+
+    _.each(options, function(value, key) {
+      this.updateEvent(eventKey, key, value);
+    }, this);
+
+    this.updateEvent(eventKey, 'end', now);
+  };
+
+  SurfNPerf.prototype.getEventData = function(eventKey, key) {
+    var eventObj = this._data.events[eventKey];
+    if(eventObj) {
+      return eventObj[key];
+    }
+  };
+
+  SurfNPerf.prototype.eventDuration = function(eventKey, options) {
+    return this._roundedDuration(this.getEventData(eventKey, 'start'), this.getEventData(eventKey, 'end'), options);
+  };
+
+  SurfNPerf.prototype.setCustom = function(key, value) {
+    this._data.custom[key] = value;
+  };
+
+  SurfNPerf.prototype.getCustom = function(key) {
+    return this._data.custom[key];
+  };
+
+  /**
+   * Total time for App Cache, DNS & TCP
+   *
+   * @returns {integer} time in ms
+   * @memberOf SurfNPerf
+   */
+  SurfNPerf.prototype.getNetworkTime = function() {
+    return this.duration('fetchStart', 'connectEnd');
+  };
+
+  /**
+   * Total time for Request & Response
+   *
+   * @returns {integer} time in ms
+   * @memberOf SurfNPerf
+   */
+  SurfNPerf.prototype.getServerTime = function() {
+    return this.duration('requestStart', 'responseEnd');
+  };
+
+  /**
+   * Total time for App Cache, DNS, TCP, Request & Response
+   *
+   * @returns {integer} time in ms
+   * @memberOf SurfNPerf
+   */
+  SurfNPerf.prototype.getNetworkLatency = function() {
+    return this.duration('fetchStart', 'responseEnd');
+  };
+
+  /**
+   * Total time to process the Response & fire the onLoad event
+   *
+   * @returns {integer} time in ms
+   * @memberOf SurfNPerf
+   */
+  SurfNPerf.prototype.getProcessingLoadTime = function() {
+    return this.duration('responseEnd', 'loadEventEnd');
+  };
+
+  /**
+   * Total time for a page to load from the time the user initiates the access of the page to the firing of the onLoad event
+   *
+   * @returns {integer} time in ms
+   * @memberOf SurfNPerf
+   */
+  SurfNPerf.prototype.getFullRequestLoadTime = function() {
+    return this.duration('navigationStart', 'loadEventEnd');
+  };
 
   return new SurfNPerf();
 
