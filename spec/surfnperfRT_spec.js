1591c37013017abc58e0cd826b7bc3ebbbb3d14f
bravominski <Minski@vagvlan536.1071.wlan.wireless-pennnet.upenn.edu>
2015-12-11 02:14:01 -0500

    duration tests updated

--- spec/surfnperfRT_spec.js
+++ spec/surfnperfRT_spec.js
@@ -129,14 +129,16 @@ define('spec/surfnperfRT_spec', [
         });
       });
       describe('when the browser supports resource timings', function() {
-        // it('returns the diff between 2 resource timing marks, not rounded if optional 4th param not included', function() {
-        //   SurfNPerfRT._resourceTiming = true;
-        //   spyOn(window.performance, 'getEntriesByName').andReturn([{
-        //     eventA: 480.04,
-        //     eventB: 490.05
-        //   }]);
-        //   expect(SurfNPerfRT.duration(name, "eventA", "eventB")).toEqual(10.01);
-        // });
+        it('returns the diff between 2 resource timing marks', function() {
+          SurfNPerfRT._resourceTiming = true;
+          spyOn(window.performance, 'getEntriesByName').andReturn([{
+            eventA: 480.04,
+            eventB: 490.05
+          }]);
+          expect(SurfNPerfRT.duration(name, "eventA", "eventB", {
+            "decimalPlaces": 2
+          })).toEqual(10.01);
+        });
         it('returns false if either eventA or eventB is 0', function() {
           SurfNPerfRT._resourceTiming = true;
           spyOn(window.performance, 'getEntriesByName').andReturn([{
@@ -147,19 +149,19 @@ define('spec/surfnperfRT_spec', [
           expect(SurfNPerfRT.duration(name, "eventA", "eventC")).toEqual(false);
           expect(SurfNPerfRT.duration(name, "eventC", "eventB")).toEqual(false);
         });
-        // it('returns the diff between 2 resource timing marks, rounded when optional 4th param decimalPlaces included', function() {
-        //   SurfNPerfRT._resourceTiming = true;
-        //   spyOn(window.performance, 'getEntriesByName').andReturn([{
-        //     eventA: 480.04,
-        //     eventB: 490.05
-        //   }]);
-        //   expect(SurfNPerfRT.duration(name, "eventA", "eventB", {
-        //     "decimalPlaces": 1
-        //   })).toEqual(10.0);
-        //   expect(SurfNPerfRT.duration(name, "eventA", "eventB", {
-        //     "decimalPlaces": 2
-        //   })).toEqual(10.01);
-        // });
+        it('returns the diff between 2 resource timing marks, rounded when optional 4th param decimalPlaces included', function() {
+          SurfNPerfRT._resourceTiming = true;
+          spyOn(window.performance, 'getEntriesByName').andReturn([{
+            eventA: 480.04,
+            eventB: 490.05
+          }]);
+          expect(SurfNPerfRT.duration(name, "eventA", "eventB", {
+            "decimalPlaces": 1
+          })).toEqual(10.0);
+          expect(SurfNPerfRT.duration(name, "eventA", "eventB", {
+            "decimalPlaces": 2
+          })).toEqual(10.01);
+        });
       });
     });
 
