ec30d0f2dd2ddc91d591c7e1e1142122c586e293
Rosmary George <rosmaryg@seas.upenn.edu>
2015-12-10 20:17:57 -0500

    some tests

--- spec/surfnperfRT_spec.js
+++ spec/surfnperfRT_spec.js
@@ -27,6 +27,31 @@ define('spec/surfnperfRT_spec', [
 
     describe('#_inList', function() {
       // Minsu
+      describe('whitelist as a key', function() {
+        options = {"whitelist" : ["A", "B", "C"]}
+        it('returns true if the given origin is listed in whitelist', function() {
+          expect(SurfNPerfRT._inList("A", options)).tobe(true);
+        });
+        it('returns false if the given origin is not listed in whitelist', function() {
+          expect(SurfNPerfRT._inList("D", options)).tobe(false);
+        });
+      });
+      describe('blackList as a key', function() {
+        options = {"blacklist" : ["A", "B", "C"]}
+        it('returns true if the given origin is not listed in blacklist', function() {
+          expect(SurfNPerfRT._inList("D", options)).tobe(true);
+        });
+        it('returns false if the given origin is listed in blacklist', function() {
+          expect(SurfNPerfRT._inList("A", options)).tobe(false);
+        });
+      });
+      describe('none specific key', function() {
+        options = {}
+        it('returns true no matter what if options does not have any key', function() {
+          expect(SurfNPerfRT._inList("A", options)).tobe(true);
+        });
+      });
+
     });
 
     describe('#_getURLOrigin', function() {
@@ -81,6 +106,15 @@ define('spec/surfnperfRT_spec', [
 
     describe('#duration', function() {
       // Minsu
+      describe('when the browser does not support resource timings', function() {
+        SurfNPerfRT._resourceTiming = false;
+        it('returns null', function() {
+          expect(SurfNPerfRT.duration("http://johnriv.github.io:3000", "A", "B", {})).toEqual(null);
+        });
+      });
+      describe('when the browser supports resource timings', function() {
+
+      });
     });
 
     describe('#start', function() {
@@ -106,6 +140,15 @@ define('spec/surfnperfRT_spec', [
 
     describe('#end', function() {
       // Minsu
+      describe('when the browser does not support resource timings', function() {
+        SurfNPerfRT._resourceTiming = false;
+        it('returns null', function() {
+          expect(SurfNPerfRT.end("http://johnriv.github.io:3000")).toEqual(null);
+        });
+      });
+      describe('when the browser supports resource timings', function() {
+
+      });
     });
 
     describe('#getFullRequestLoadTime', function() {
@@ -123,6 +166,15 @@ define('spec/surfnperfRT_spec', [
 
     describe('#getNetworkTime', function() {
       // Minsu
+      describe('when the browser does not support resource timings', function() {
+        SurfNPerfRT._resourceTiming = false;
+        it('returns null', function() {
+          expect(SurfNPerfRT.getNetworkTime("http://johnriv.github.io:3000", {})).toEqual(null);
+        });
+      });
+      describe('when the browser supports resource timings', function() {
+
+      });
     });
 
     describe('#getServerTime', function() {
@@ -140,6 +192,15 @@ define('spec/surfnperfRT_spec', [
 
     describe('#getBlockingTime', function() {
       // Minsu
+      describe('when the browser does not support resource timings', function() {
+        SurfNPerfRT._resourceTiming = false;
+        it('returns null', function() {
+          expect(SurfNPerfRT.getBlockingTime("http://johnriv.github.io:3000", {})).toEqual(null);
+        });
+      });
+      describe('when the browser supports resource timings', function() {
+
+      });
     });
 
   });
