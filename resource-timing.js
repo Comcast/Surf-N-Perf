afdc7402e42c4199fc3411f06d125d5f0aac9613
John Riviello <john_riviello@comcast.com>
2022-10-07 16:20:58 -0400

    it's 2022

--- resource-timing.js
+++ resource-timing.js
@@ -1,6 +1,6 @@
 /*!
  * @license
- * Copyright 2015-2021 Comcast Cable Communications Management, LLC
+ * Copyright 2015-2022 Comcast Cable Communications Management, LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
